51b0c38e0861f1090095f5dcb6d449a3b9414a7f
Brendan Kennedy <brendan.fj.kennedy@gmail.com>
2021-10-20 15:37:48 -0400

    got the code to add the next recipe

--- src/App.js
+++ src/App.js
@@ -3,78 +3,69 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    isAddRecipeFormDisplayed: false, 
-    recipes: [], 
+    isAddRecipeFormDisplayed: false,
+    recipes: [],
     newRecipeName: '',
-    newRecipeInstructions: ""
+    newRecipeInstructions: ''
   }
-
-
-
-handleChange = (event) => {
-  const target = event.target
-  const name = target.name
-
-  this.setState({[name]: target.value});
-
-}
-
-submitRecipe = (event) => {
-  event.preventDefault()
-  this.setState({...this.state.recipes, recipes: [
-      {
-        name: this.state.newRecipeName,
-        instructions :this.state.newRecipeInstructions
-      }
-    ]
-  })
-}
 
   toggleAddRecipeForm = () => {
     this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
   }
 
-  recipeList = (props) => { 
-    props.map((recipe) => (<li>{recipe.name}</li>))
+  submitRecipe = (event) => {
+    event.preventDefault();
+    let newRecipe = {
+      name: this.state.newRecipeName,
+      instructions: this.state.newRecipeInstructions,
+    }
 
+this.setState({recipes: [...this.state.recipes, newRecipe]})
+console.log(this.state.recipes)
+this.toggleAddRecipeForm();
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+
+    this.setState({[name]: target.value});
   }
 
   render(){
   const addNewRecipeForm = (
-      <form id="recipe-form" onSubmit={this.submitRecipe} >
+      <form id="recipe-form" onSubmit={this.submitRecipe}>
         <label htmlFor="newRecipeName">Recipe name: </label>
-        <input type="text" 
-        name = 'newRecipeName' 
-        id="newRecipeName" 
-        onChange={this.handleChange} 
-        value={this.state.newRecipeName}/>
+        <input type="text"
+          id="newRecipeName"
+          name="newRecipeName"
+          onChange={this.handleChange}
+          value={this.state.newRecipeName} />
         <label htmlFor="newRecipeInstructions">Instructions:</label>
-        <textarea name = 'newRecipeInstructions'
-        id="newRecipeInstructions" 
-        placeholder="write recipe instructions here..."
-        onChange={this.handleChange}
-        value={this.state.newRecipeInstructions} />
+        <textarea id="newRecipeInstructions"
+          name="newRecipeInstructions"
+          placeholder="write recipe instructions here..."
+          onChange={this.handleChange}
+          value={this.state.newRecipeInstructions} />
         <input type="submit" />
       </form>
     )
 
-    return (
-      <div className="App">
-        <h1 className="App-header">My Recipes</h1>
-        {
-          this.state.isAddRecipeFormDisplayed
-          ? addNewRecipeForm
-          : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
-        }
-        {
-        this.state.recipes.length > 0 ?
-        <ul>
-          {this.recipeList(this.state.recipes)}
-        </ul> :
-        <p>There are no recipes to list.</p>
-        }
-      </div>
-    )
+return (
+  <div className="App">
+    <h1 className="App-header">My Recipes</h1>
+    {
+      this.state.isAddRecipeFormDisplayed
+      ? addNewRecipeForm
+      : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
+    }
+  {
+    this.state.recipes.length > 0 ?
+    <ul name="recipeList">{this.state.recipes.map((recipe) => {return(<li key={recipe.name}>{recipe.name}</li>)})}</ul> :
+    <p>There are no recipes to list.</p>
+  }
+  </div>
+)
   }
 }
 
